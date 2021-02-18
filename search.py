a271e70153c14db999bbfe6940e229ed4a5b8197
Kourosh Alizadeh <kcalizadeh@gmail.com>
2021-02-18 09:43:56 -0700

    fixing none error

--- search.py
+++ search.py
@@ -43,53 +43,54 @@ app.layout = html.Div([
               Input(component_id="text-search-bar", component_property="n_submit"),
               Input(component_id="source-selection", component_property="value")],
               [State(component_id="text-search-bar", component_property="value")])
-def search_df(n_clicks, n_submit, source, text): 
-    if n_clicks < 1 and n_submit < 1:
-        return [html.Br(), html.P('Search results will appear here.')]
-    if n_clicks > 0 or n_submit > 0:
-            with open(f'slice_pickles/{source}_slice.pkl', 'rb') as df_pkl:
-                df = pickle.load(df_pkl)
-                results = df[df['SENTENCE'].str.contains('(?i)\s'+text+'\s')].copy()
-                results = results.sample(len(results))
-            if len(results) == 0:
-                return 'Sorry, that word was not found in the selected source.'
-            else:
-                try:
-                    to_present = dash_table.DataTable(
-                                                    id='table',
-                                                    style_cell={
-                                                                    'whiteSpace': 'normal',
-                                                                    'height': 'auto',
-                                                                    'lineHeight': '15px',
-                                                                    'textAlign': 'left',
-                                                                    'padding': '5px'
+def search_df(n_clicks, n_submit, source, text):
+    if source: 
+        if n_clicks < 1 and n_submit < 1:
+            return [html.Br(), html.P('Search results will appear here.')]
+        if n_clicks > 0 or n_submit > 0:
+                with open(f'slice_pickles/{source}_slice.pkl', 'rb') as df_pkl:
+                    df = pickle.load(df_pkl)
+                    results = df[df['SENTENCE'].str.contains('(?i)\s'+text+'\s')].copy()
+                    results = results.sample(len(results))
+                if len(results) == 0:
+                    return 'Sorry, that word was not found in the selected source.'
+                else:
+                    try:
+                        to_present = dash_table.DataTable(
+                                                        id='table',
+                                                        style_cell={
+                                                                        'whiteSpace': 'normal',
+                                                                        'height': 'auto',
+                                                                        'lineHeight': '15px',
+                                                                        'textAlign': 'left',
+                                                                        'padding': '5px'
+                                                                    },
+                                                        
+                                                        style_data_conditional=[
+                                                            {
+                                                                'if': {'column_id': 'TITLE'}, 
+                                                                    'min_width':'200px'
                                                                 },
-                                                    
-                                                    style_data_conditional=[
-                                                        {
-                                                            'if': {'column_id': 'TITLE'}, 
-                                                                'min_width':'200px'
-                                                            },
-                                                            {
-                                                            'if': {'column_id':'AUTHOR'},
-                                                                'min_width':'80px'
-                                                            },
-                                                            {
-                                                            'if': {'column_id':'SCHOOL'},
-                                                                'min_width':'80px'
-                                                            }
-                                                    ],
-                                                    data=results.to_dict('records'),
-                                                    columns=[{"name": i, "id": i} for i in df.columns],
-                                                    page_action='native',
-                                                    page_current= 0,
-                                                    page_size= 15,
-                                                )
-            
+                                                                {
+                                                                'if': {'column_id':'AUTHOR'},
+                                                                    'min_width':'80px'
+                                                                },
+                                                                {
+                                                                'if': {'column_id':'SCHOOL'},
+                                                                    'min_width':'80px'
+                                                                }
+                                                        ],
+                                                        data=results.to_dict('records'),
+                                                        columns=[{"name": i, "id": i} for i in df.columns],
+                                                        page_action='native',
+                                                        page_current= 0,
+                                                        page_size= 15,
+                                                    )
                 
-                    return [html.Br(), html.P(f'Showing {len(results)} results for "{text}" in {source}.'), to_present]
-                except:
-                    return 'Sorry, something went wrong.'
+                    
+                        return [html.Br(), html.P(f'Showing {len(results)} results for "{text}" in {source}.'), to_present]
+                    except:
+                        return 'Sorry, something went wrong.'
 
 
 
